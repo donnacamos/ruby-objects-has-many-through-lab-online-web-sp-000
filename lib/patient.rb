2051ffa7ae8e4e360136672f387d980355c522a2
Donna Amos <donnacamos.developer@gmail.com>
2019-03-20 19:21:43 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,35 +1,26 @@
 class Patient
-  
-  attr_accessor :name, :appointment, :doctor 
-  
-  @@all = [] 
-  
-  def initialize(date) 
-    @name = name 
-    @@all << self 
-  end 
-  
-  def new_appointment(doctor, date) 
-    Appointment.new(self, doctor, date) 
-    end 
-  end 
-  
-  def appointments 
-    Appointment.all.select do |appointment| 
-      appointment.patient == self 
-  end 
-  
-  def doctors 
-    Appointment.all.map do |appointment| 
-      appointment.doctor  
-    end 
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-  
-    
-end 
+
+   attr_reader :name, :appointment, :doctor
+
+   def initialize(name)
+    @name = name
+  end
+
+   def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+
+   def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+   def doctors
+    Appointment.all.map do |appointment|
+      appointment.doctor
+    end
+  end
+end
       
   
