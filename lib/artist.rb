50e8d3584b067c7754b2c67f8bcaadf9cbeb372d
Donna Amos <donnacamos.developer@gmail.com>
2019-03-20 18:15:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,8 +13,9 @@ class Artist
     @name 
   end 
   
-  def new_song(name, genre) 
-    Song.new(self, name, genre) 
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    self.songs.last
   end
   
   def songs 
