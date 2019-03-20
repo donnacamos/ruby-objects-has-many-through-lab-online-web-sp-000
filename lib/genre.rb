12c84bea2116815a4a2282daaa9ef689a2d2e4f9
Donna Amos <donnacamos.developer@gmail.com>
2019-03-20 18:24:24 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,28 +1,22 @@
-class Genre 
-  
-  @@all = [] 
-  
-  attr_reader :name, :genre 
-  attr_accessor :song  
-  
-  def initialize(name) 
-    @name = name 
-  end 
-  
-  def songs 
-    Song.all.select do |song| 
-      song.genre == self 
-    end 
+class Genre
+
+   attr_reader :name, :genre
+  attr_accessor :song
+
+   def initialize(name)
+    @name = name
   end
-  
-  def artists 
-    Song.all.map do |song|  
-      song.artist  
-    end 
+
+   def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
   end
-  
-  def self.all 
-    @@all 
-  end 
-end 
+
+   def artists
+    Song.all.map do |song|
+      song.artist
+    end
+  end
+end
     
