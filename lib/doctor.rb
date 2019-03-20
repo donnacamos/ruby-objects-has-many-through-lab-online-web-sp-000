0e11f9f6de5d21a6c78c4d26ce7e0973e18468cd
Donna Amos <donnacamos.developer@gmail.com>
2019-03-20 18:59:20 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,8 +14,7 @@ class Doctor
   end 
   
   def new_appointment(patient, date) 
-    @appointments << appointment 
-    apointment.doctor = self 
+    Appointment.new
   end 
   
   def appointments 
